2aeb254d00c9328c056e861baba72c3415103a8a
Simon Kirsten <simon.kirsten@stud.h-da.de>
2019-11-03 20:08:42 +0100

    Bumped version to 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pymdown-env",
-    version="1.0.0",
+    version="1.0.1",
     author="Simon Kirsten",
     author_email="simon.kirsten@stud.h-da.de",
     description="pymdown extension to inline replace environment variables",
